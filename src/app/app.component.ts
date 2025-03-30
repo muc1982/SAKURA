f712107278875484bd1e851f2f2492594a4be855
Yasin <yasin.sun@t-online.de>
2025-03-30 21:20:08 +0200

    Mein erster Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landingPage/landingPage.component'
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-landingPage',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <section>
+      <h1 class="fontRaleway">SAKURA RAMEN</h1>
+      <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
+    </section>
+  `,
+
+  styleUrls: ['./landingPage.component.scss']
+})
+export class LandingPageComponent {}
+
