91acfdd58bbc9cd6bf6fda12377071c2dc4e66fc
Yasin <yasin.sun@t-online.de>
2025-04-07 22:57:24 +0200

    BUGFIX

---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingPageComponent } from './landingPage/landingPage.component';
+import { OurMenuComponent } from './our-menu/our-menu.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
+
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [CommonModule, LandingPageComponent, OurMenuComponent, HowToOrderComponent],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss'
+})
+export class MainContentComponent {
+
+}
